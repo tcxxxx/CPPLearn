07dd1a85de8a09d68f6759366e94fc8bec33b30b
tcxxxx <luffyrocks@sina.com>
2019-03-13 09:47:52 -0400

    a simpler example && considering volatile member function

---
+++ segFault_uninitPtr/simpler_example.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+/*
+	Example:
+		Segmentation Fault caused by using uninitialized pointer.
+*/
+
+class T
+{
+	public:
+		T(): i(2) { std::cout << "(T) construction" << std::endl; }
+		~T() { std::cout << "(T) destruction" << std::endl; }
+
+	public:
+		int i;
+		void func1()
+		{
+			std::cout << "func() start" << std::endl;
+			std::cout << "func() end" << std::endl;
+		}
+
+		void func2()
+		{
+			std::cout << "func() start" << std::endl;
+			i = 1;
+			std::cout << "func() end" << std::endl;
+		}
+
+		virtual void func3()
+		{
+			std::cout << "func() start" << std::endl;
+			std::cout << "func() end" << std::endl;
+		}
+};
+
+/*
+	TEST: accessing two non-volatile functions:
+		func1 does nothing but std::cout;
+		func2 tries to operate on the member variable i;
+*/
+void test(bool init_=false)
+{
+	T * ptr_ = NULL;
+	if (init_)
+		ptr_ = new T;
+
+	std::cout << "accessing func1:" << std::endl;
+	ptr_->func1();
+	std::cout << "accessing func2:" << std::endl;
+	ptr_->func2();
+	delete ptr_;
+}
+
+/*
+	TEST2: accessing a volatile function member.
+*/
+void test2(bool init_=false)
+{
+	T * ptr_ = NULL;
+	if (init_)
+		ptr_ = new T;
+
+	ptr_->func3();
+	delete ptr_;
+}
+
+int main()
+{
+	// std::cout << "ptr initialized" << std::endl;
+	// test(true);
+	// std::cout << std::endl;
+
+	std::cout << "ptr uninitialized" << std::endl;
+	// accessing a non-volatile member function
+	test(false); 
+	test2(false);
+	return 0;
+}
